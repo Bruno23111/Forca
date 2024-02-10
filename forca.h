d3a0e80461544cd6c54c565840fae35e41ab0f3e
Bruno Bomfim Lima <81599298+biscoitindogral@users.noreply.github.com>
2024-02-10 10:57:27 -0300

    FINALCURSO

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_DA_PALAVRA 20
+
+int chuteserrados();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+int acertou();
+void escolhepalavra();
+int enforcou();
+void escolhepalavra();
+void adicionapalavra();
